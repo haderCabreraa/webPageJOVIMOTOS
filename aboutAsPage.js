4ac71ce45de8af1c61b943d2cd2a7c6defdb2082
HaderCabrera <hader.cabrera96@gmail.com>
2024-01-14 10:55:56 -0500

    tercera pagina 70%

---
+++ aboutAsPage.js
@@ -0,0 +1,4 @@
+    //ESCUCHA DE BOTON HAMBURGUESA
+    document.querySelector('.menu-btn').addEventListener('click', () => {
+        document.querySelector('.nav-menu').classList.toggle("show")
+    });
